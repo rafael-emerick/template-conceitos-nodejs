712df3171f20cf2f66b4cdf04b1c3874a8bb3cb7
rafael-emerick <rafaelemerick5@gmail.com>
2020-11-01 14:47:01 -0300

    Subindo as alterações no projeto

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -10,24 +9,73 @@ app.use(cors());
 
 const repositories = [];
 
+function validateId(request, response, next){
+  const { id } = request.params;
+  if(!isUuid(id)){
+    return response.status(400).json({ error: 'Invalid id'});
+  }
+
+  next();
+}
+
+app.use('/repositories/:id', validateId)
+
 app.get("/repositories", (request, response) => {
-  // TODO
+    return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body
+  const repositorie = {
+    id: uuid(),
+    title, 
+    url,
+    techs,
+    likes: 0,
+  }
+  repositories.push(repositorie)
+  return response.json(repositorie)
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body
+  const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id)
+  if(repositorieIndex < 0){
+    return response.status(400).json({ error: 'Invalid repositorie id'});
+  }
+  const repositorie = {
+    id,
+    title, 
+    url,
+    techs,
+    likes: repositories[repositorieIndex].likes,
+  };
+  repositories[repositorieIndex] = repositorie
+  return response.json(repositorie)
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+   const { id } = request.params
+   const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+   if(repositorieIndex < 0){
+      return response.status(400).json({ error: 'Invalid repositorie id.'});
+   }
+   repositories.splice(repositorieIndex, 1);
+   return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+   const { id } = request.params;
+   const repositorieIndex = repositories.findIndex(repositorie => repositorie.id === id);
+   if(repositorieIndex < 0){
+    return response.status(400).json({ error: 'Invalid repositorie id.'});
+   }
+   const repositorie = repositories[repositorieIndex];
+   repositories[repositorieIndex].likes = repositorie.likes + 1
+   return response.json(repositorie);
 });
+
+//app.listen(3334)
 
 module.exports = app;
